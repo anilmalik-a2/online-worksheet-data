815f7d7ee1ae551ec33fca25e85deab6500aa749
Anil <anilmalik50@gmail.com>
2021-04-12 14:20:13 +0530

    github pages extension installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDCg-AfxxmV4LmquhLowId1CZF56xbNMAQ",
+    authDomain: "online-worksheets-data.firebaseapp.com",
+    projectId: "online-worksheets-data",
+    storageBucket: "online-worksheets-data.appspot.com",
+    messagingSenderId: "1056233139291",
+    appId: "1:1056233139291:web:365066f892dee60a9ae37e",
+    measurementId: "G-BCKQ12TQVD"
+  }
 };
