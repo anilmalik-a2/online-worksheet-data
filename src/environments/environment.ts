8e9282be196b3866a3faf0e6907a7944c0f227d9
anilmalik <anilmalik.sv.a2@gmail.com>
2021-08-05 08:11:24 +0530

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDCg-AfxxmV4LmquhLowId1CZF56xbNMAQ",
-    authDomain: "online-worksheets-data.firebaseapp.com",
-    projectId: "online-worksheets-data",
-    storageBucket: "online-worksheets-data.appspot.com",
-    messagingSenderId: "1056233139291",
-    appId: "1:1056233139291:web:365066f892dee60a9ae37e",
-    measurementId: "G-BCKQ12TQVD"
+    apiKey: 'AIzaSyDCg-AfxxmV4LmquhLowId1CZF56xbNMAQ',
+    authDomain: 'online-worksheets-data.firebaseapp.com',
+    projectId: 'online-worksheets-data',
+    storageBucket: 'online-worksheets-data.appspot.com',
+    messagingSenderId: '1056233139291',
+    appId: '1:1056233139291:web:365066f892dee60a9ae37e',
+    measurementId: 'G-BCKQ12TQVD'
   }
 };
 
